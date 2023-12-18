fbc2249bf138c581ba665473271d5a6e3bbd6608
Berlindraft <149296545+Berlindraft@users.noreply.github.com>
2023-12-18 19:23:58 +0800

    Add files via upload

---
+++ tulips4herrr.js
@@ -0,0 +1,54 @@
+/* of course ako ang ga structure but i lack in skill pa to fully make this js document */
+/* Generate random box shadow values */
+function generateRandomBoxShadow(n) {
+    let boxShadow = '';
+    for (let i = 0; i < n; i++) {
+      boxShadow += `${getRandomValue(2000)}px ${getRandomValue(2000)}px #FFFFFF`;
+      if (i < n - 1) {
+        boxShadow += ', ';
+      }
+    }
+    return boxShadow;
+  }
+  
+  /* Get a random value up to a maximum */
+  function getRandomValue(max) {
+    return Math.floor(Math.random() * max) + 1;
+  }
+  
+  /* Assign generated box shadows to variables */
+  var shadowsSmall = generateRandomBoxShadow(700);
+  var shadowsMedium = generateRandomBoxShadow(200);
+  var shadowsBig = generateRandomBoxShadow(100);
+
+document.getElementById('stars').style.boxShadow = shadowsSmall;
+document.getElementById('stars2').style.boxShadow = shadowsMedium;
+document.getElementById('stars3').style.boxShadow = shadowsBig;
+
+let flowerCount = 0;
+
+document.addEventListener('click', function (event) {
+  const maxFlowers = 10; // Set the maximum number of flowers allowed
+
+  if (flowerCount < maxFlowers) {
+    // Get the original flower
+    const originalFlower = document.getElementById('originalFlower');
+
+    // Clone the original flower
+    const clone = originalFlower.cloneNode(true);
+
+    // Remove the ID to avoid duplicate IDs in the document
+    clone.removeAttribute('id');
+
+    // Increment the flower count
+    flowerCount++;
+
+    // Set an offset for the clone
+        // Set the position for the clone
+        clone.style.left = `${event.clientX}px`; // Use clientX to get the X coordinate of the click event
+        clone.style.top = `${event.clientY}px`; // Use clientY to get the Y coordinate of the click event
+
+    // Append the clone after the original flower
+    originalFlower.parentNode.insertBefore(clone, originalFlower.nextSibling);
+  }
+});
